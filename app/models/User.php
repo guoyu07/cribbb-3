7930e529cbb96ec9acc96d95dffb08cc4b43609a
Ian Simmons <isimmons33@gmail.com>
2014-01-31 16:35:34 -0500

    create users table migration and setup User model

--- app/models/User.php
+++ app/models/User.php
@@ -5,48 +5,69 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password');
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+    /**
+     * The attributes fillable for mass assignment protection.
+     *
+     * @var array
+     */
+    protected $fillable = ['username', 'email', 'password'];
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+    	return $this->getKey();
+    }
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+    	return $this->password;
+    }
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+    	return $this->email;
+    }
+
+    /**
+     * Force hashing of the password on save and/or create
+     *
+     * @param $value string
+     * @return void
+     */
+    public function setPasswordAttribute($value)
+    {
+        //make sure not to double hash in case allready hashed
+        if ( ! Hash::needsRehash($value)) {
+            $this->attributes['password'] = Hash::make($value);    
+        }        
+    }
 
 }
